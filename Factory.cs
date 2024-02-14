b113b48da87a01153c105b7580150f8be48cca77
HoroshoNaVolge <44640207+HoroshoNaVolge@users.noreply.github.com>
2024-02-14 10:13:40 +0300

    Classes updated

--- Factory.cs
+++ Factory.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace dotnetLearning
 {
+    /// <summary>
+    /// Завод
+    /// </summary>
     internal class Factory
     {
+        public int Id { get; private set; }
+        public string Name { get; private set; }    
+        public string Description { get; private set; }
+
+        public Factory(int id, string name, string description)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+        }
     }
 }
--- Tank.cs
+++ Tank.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace dotnetLearning
 {
+    /// <summary>
+    /// Резервуар
+    /// </summary>
     internal class Tank
     {
+        public int Id {  get; private set; }
+        public int Volume { get; set; }
+        public int MaxVolume { get; private set; }
+
+        public string Name {  get; private set; }
+        public string Description { get; private set; }
+
+        private Unit _unit;
+
+
+        public Tank(int volume, int maxVolume, string name, string description) 
+        {
+            Volume= volume;
+            MaxVolume= maxVolume;
+            Name= name;
+            Description= description;
+        }
     }
 }
